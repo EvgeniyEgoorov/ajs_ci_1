5e497994e558cb9ca4510f9d81b14078813e2200
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-12-22 18:07:31 +0800

    fix: unquoted properties in app test

--- src/js/__tests__/app-test.js
+++ src/js/__tests__/app-test.js
@@ -1,11 +1,10 @@
-import healthIndicator from "../app" 
+import healthIndicator from '../app';
 
 test.each([
-    [{'name': 'char1', 'health': 90,}, 'healthy'],
-    [{'name': 'char2', 'health': 45}, 'wounded'],
-    [{'name': 'char3', 'health': 5}, 'critical'],
-])
-('testing health status for %o', (object, expected) => {
+    [{ name: 'char1', health: 90 }, 'healthy'],
+    [{ name: 'char2', health: 45 }, 'wounded'],
+    [{ name: 'char3', health: 5 }, 'critical'],
+])('testing health status for %p', (object, expected) => {
     const result = healthIndicator(object);
     expect(result).toBe(expected);
 });
